2f9dfe0d55605788f8d436e0ae3d1c531b447cc7
ally-sorloth <allysorloth@gmail.com>
2020-12-10 22:46:15 +0300

    create routers, controllers and models

---
+++ controllers/AuthController.js
@@ -0,0 +1,13 @@
+
+
+exports.authRegister = (req, res) => {
+    //TODO: register function
+    res.send("Register Completed");
+};
+
+
+exports.authLogin = (req, res) => {
+    //TODO: Auth.
+    //TODO: Login Func.
+    res.send("Login Completed");
+}
--- routes/router.js
+++ routes/router.js
@@ -2,17 +2,31 @@ const express = require("express");
 
 const router = express.Router();
 
+const AuthRouter = require("./AuthRouter");
+const ProfileRouter = require("./ProfileRouter");
+const BookRouter = require("./BookRouter");
 
-router.get ("/", (req, res) => {
-    console.log("Home Page");
-    res.send("Home Page")
-});
+// Only api endpoint
 
-router.get("/api", (req, res) => {
-    console.log("API Page");
-    res.send("API Page")
-});
+/**
+ * @route /api/auth
+ * @desc Route for Auth 
+ */
 
+router.use("/auth", AuthRouter);
+
+/**
+ * @route /api/profile
+ * @desc Route for Profile 
+ */
+
+router.use("/profile", ProfileRouter);
+
+/**
+ * @route /api/book
+ * @desc Route for Book 
+ */
+router.use("/book", BookRouter);
 
 
 
