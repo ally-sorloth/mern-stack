3a15bc8f0539252d71971cbae7adec22be05664b
ally-sorloth <allysorloth@gmail.com>
2020-12-13 19:32:35 +0300

    add client react

--- server.js
+++ server.js
@@ -12,6 +12,17 @@ app.use(express.json());
 
 app.use("/api", router);
 
+
+// production
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+}
+
+
+
 app.listen(process.env.PORT, () => {
     console.log(`I am listening on port ${process.env.PORT}` )
 });
